0b81589ab4609d9ecb8b53a045cce306c6625563
JackMagee21 <mt.j07@yahoo.com>
2026-09-22 21:50:11 +0100

    Added cmd_info and the command "info"

--- cmds/cmd.py
+++ cmds/cmd.py
@@ -1,10 +1,13 @@
 from cmds.help import help_cmd
 from cmds.ls import cmd_ls
+from cmds.info import cmd_info
 
 def cmd(repo, command, parameters=""):
     if command == "help":
         print(help_cmd())
     elif command == "ls":
         print(cmd_ls(repo, parameters))
+    elif command == "info":
+        print(cmd_info(repo))
     else:
         print("Sorry, command not found")
