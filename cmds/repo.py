591ffb0524e90023a81cb87adef583e8fbbf0352
JackMagee21 <mt.j07@yahoo.com>
2026-09-23 14:42:16 +0100

    Added a repo error case, just adding a error message if a user doesnt put a repo title after typing the repo command

--- cmds/repo.py
+++ cmds/repo.py
@@ -4,6 +4,9 @@ from RepoData import set_repo
 git = Github()
 
 def cmd_repo(repo, parameters=""):
+    if parameters == "":
+        print("Error: no repo name added, enter (for example) Author/Repo_name")
+
     if repo.switch_repo(parameters) == False:
         print("Repo cannot be found")
     
